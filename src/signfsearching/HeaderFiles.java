358a53f463a92d2a95e9de69b2d187702b8dd0c5
Ruslan Raietskyi <raietskiy@gmail.com>
2014-11-24 20:14:09 +0200

    added check application arguments
    for null

--- src/signfsearching/HeaderFiles.java
+++ src/signfsearching/HeaderFiles.java
@@ -7,14 +7,20 @@ import java.util.ArrayList;
 public class HeaderFiles {
 
 	public static void main(String[] args) throws IOException {
-		
-		//final String FILESTYPE = "exe";
-		final String FILESTYPE = "apk";
+
+		final String FILESTYPE = "exe";
+		// final String FILESTYPE = "apk";
 		// requested file path
 		String reqFilePath = null;
 		if (args.length < 2) {
-			System.out.println("Missing path of folders!!!");
+			System.err.println("Missing path of folders!!!");
 			System.exit(1);
+		}
+		if ((args[0].equalsIgnoreCase("null"))
+				|| (args[1].equalsIgnoreCase("null"))) {
+			System.err.println("Agument(s) cannot be a NULL!");
+			System.exit(1);
+
 		} else {
 
 			File inpFldr = new File(args[0].toString());
@@ -26,20 +32,19 @@ public class HeaderFiles {
 				reqFilePath = SearchFileSignature.searchSigFile(f, FILESTYPE);
 
 				if (reqFilePath == "-1") {
-					
+
 					System.err.println("File doesn't exist!");
-					
+
 				} else if (reqFilePath == "-2") {
-					
+
 					// "-2" - the returns code processing file, doesn't compare
 					// to need suffix
 					;
 				} else {
-					
+
 					CopyFile.copyFile(reqFilePath, args[1]);
-					
+
 				}
-				
 
 			}
 
